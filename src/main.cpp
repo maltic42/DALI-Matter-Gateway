9578a8a3d2a60d6d9c750a28208e67690bfa080a
Malte Rudolf <rudolf@toad.de>
2026-09-17 09:24:32 +0200

    Add copyright and license information to source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,36 @@
+/*
+
+Copyright (C) 2026 Malte Rudolf
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+License
+
+This project is licensed under the GNU General Public License v3.0 (GPL-3.0).
+
+You are free to:
+use the software for private or commercial purposes study and modify the source code redistribute the software distribute modified versions of the software
+Any redistribution of this software or modified versions must comply with the terms of the GNU General Public License v3.0.
+
+Third-Party Components
+
+This project uses third-party software and libraries that are distributed under their respective licenses.
+The applicable licenses and copyright notices of third-party components remain in effect. Please refer to the respective source code and documentation for details.
+
+Disclaimer
+
+This software is provided "AS IS", without warranty of any kind, express or implied.
+The author provides no guarantee that the software is free of defects or suitable for any particular purpose. The author shall not be responsible for any damage, data loss, hardware damage, financial loss, or other consequences resulting from the use of this software, to the extent permitted by applicable law.
+
+Support and Maintenance
+
+This project is provided without any obligation to provide support, maintenance, updates, bug fixes, or other services.
+There is no guarantee that issues reported through GitHub Issues, Discussions, or other communication channels will be addressed.
+For the complete terms and conditions, please refer to the LICENSE file containing the GNU General Public License v3.0.
+
+*/
+
+
 #include <Arduino.h>
 #include <Matter.h>
 #include <WiFi.h>
